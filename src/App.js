a74730a38c90fc3d2b7def069bd93a4aaedf0655
Saketh D <saketh1111@icloud.com>
2024-10-03 15:11:14 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+import RestaurantMenu from  "./components/RestaurantMenu"
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import UserContext from "./utils/UserContext";
+ /**
+  * Header
+  * - Logo
+  * - Nav Items
+  * Body
+  * - Search
+  * - restaurant catlogue
+  *     - Name of Restaurant
+  *     - Cuisunes, star rating , delivery etc.
+  *     
+  * Footer
+  * - Copyroght
+  * - Links
+  * - Address
+  * - Contact
+  */
+ const AppLayout = () => {
+
+    const [userName, setUserName]=useState();
+    useEffect(()=>{
+
+        const data = {
+            name: "Saketh Dasavathini",
+        }
+        setUserName(data.name);
+
+    },[])
+    return (
+        <UserContext.Provider value={{loggedInUser:userName, setUserName}}>
+        <div className="app">
+            <Header />
+            {console.log("App Render")}
+            <Outlet />
+
+        </div>
+        </UserContext.Provider>
+    // Body
+    // Footer
+    )
+}
+ const appRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <AppLayout/>,
+        children:[
+            {
+                path: "/",
+                element: <Body/>,
+            },
+            {
+                path: "/about",
+                element: <About/>,
+            },
+            {
+                path: "/contact",
+                element: <Contact/>,
+            },
+            {
+                path:"/restaurants/:resId",
+                element: <RestaurantMenu/>
+            }
+
+        ],
+        errorElement: <Error/>
+    },
+    
+ ])
+ const root = ReactDOM.createRoot(document.getElementById("root"));
+ root.render(<RouterProvider router={appRouter} />);
